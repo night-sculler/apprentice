b6c441143e28c4298bef088d32f2785052cef8ae
houlongjie <houlongjie@jd.com>
2021-07-12 11:35:47 +0800

    shiro

---
+++ shiro/src/main/java/com/night/sculler/shiro/chapter2/MyToken.java
@@ -0,0 +1,12 @@
+package com.night.sculler.shiro.chapter2;
+
+import org.apache.shiro.authc.UsernamePasswordToken;
+
+/**
+ * @author night-sculler
+ * @version 1.0.0.release
+ * @date 2021/7/4
+ */
+public class MyToken extends UsernamePasswordToken {
+
+}
