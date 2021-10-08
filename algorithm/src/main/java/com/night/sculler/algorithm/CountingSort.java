930c973c29759afda4d2746d024698aa905c1e03
houlongjie <houlongjie@jd.com>
2021-10-08 19:00:02 +0800

    计数排序

--- algorithm/src/main/java/com/night/sculler/algorithm/CountingSort.java
+++ algorithm/src/main/java/com/night/sculler/algorithm/CountingSort.java
@@ -10,13 +10,17 @@ import java.util.Arrays;
  * @date 2021/10/8
  */
 public class CountingSort {
-    public static void sortIntArrayAsc(int[] intArr) {
+    /**
+     * 对int数组做升序排序
+     *
+     * @param intArr int数组
+     */
+    private static void sortIntArrayAsc(int[] intArr) {
         int length = intArr.length;
         if (length <= 1) {
             return;
         }
-
-        //求出数组的最小最大值
+        //求出最大最小元素
         int min = intArr[0];
         int max = intArr[0];
         for (int element : intArr) {
@@ -27,29 +31,28 @@ public class CountingSort {
                 max = element;
             }
         }
-
-        //计数数组
-        int[] numCountArr = new int[max - min + 1];
-        //统计原数组中每个元素的个数，其元素减去偏移量作为计数数组的下标，个数作为计数数组的元素
-        //切记计数数组的下标是原数组元素减去偏移量得到的，偏移量为原数组的最小值
+        //计数数组，统计原数组中每个元素存在多少个(做了偏移处理，偏移量为原数组的最小元素)
+        //原数组中的元素减去偏移量，作为计数数组的下标，原数组元素个数作为计数数组的元素
+        int[] countNumArr = new int[max - min + 1];
         for (int element : intArr) {
-            numCountArr[element - min]++;
+            countNumArr[element - min]++;
         }
-        //前缀数组(将计数数组当前缀数组使用，节省空间)
-        for (int i = 1; i < numCountArr.length; i++) {
-            numCountArr[i] = numCountArr[i] + numCountArr[i - 1];
+        //前缀和数组，统计原数组中某个元素及其前面的元素总共多少个
+        for (int i = 1; i < countNumArr.length; i++) {
+            countNumArr[i] = countNumArr[i] + countNumArr[i - 1];
         }
-
         //零时排序数组
-        int[] tmpArr = new int[length];
-        //遍历原数组，用原数组中的元素作为下标，到前缀数组中取数，这个数就是该元素在排序数组中对应的顺序
+        int[] tmpSortArr = new int[length];
+        //从后往前取出原数组中的元素
+        //作为前缀和数组的下标
+        //取出前缀和数组对应的元素
+        //该元素值就是原数组元素对应零时排序数组的位置
         for (int i = length - 1; i >= 0; i--) {
             int element = intArr[i];
-            int tmpArrIndex = numCountArr[element - min] - 1;
-            tmpArr[tmpArrIndex] = element;
+            int tmpSortArrIndex = countNumArr[element - min] - 1;
+            tmpSortArr[tmpSortArrIndex] = element;
         }
-        //将有序元素copy到原数组
-        System.arraycopy(tmpArr, 0, intArr, 0, length);
+        System.arraycopy(tmpSortArr, 0, intArr, 0, length);
     }
 
     public static void main(String[] args) {
--- algorithm/src/main/java/com/night/sculler/algorithm/SortPractice.java
+++ algorithm/src/main/java/com/night/sculler/algorithm/SortPractice.java
@@ -12,42 +12,47 @@ import java.util.Arrays;
 public class SortPractice {
 
     private static void sortIntArrayAsc(int[] intArr) {
-        quickSort(intArr, 0, intArr.length - 1);
-    }
-
-    private static void quickSort(int[] intArr, int low, int high) {
-        if (low < high) {
-            int index = partition(intArr, low, high);
-            quickSort(intArr, low, index - 1);
-            quickSort(intArr, index + 1, high);
+        int length = intArr.length;
+        if (length <= 1) {
+            return;
         }
-    }
-
-    private static int partition(int[] intArr, int low, int high) {
-        int pivot = intArr[low];
-        int start = low;
-        while (low < high) {
-            while (low < high && intArr[high] >= pivot) {
-                high--;
+        //求出最大最小元素
+        int min = intArr[0];
+        int max = intArr[0];
+        for (int element : intArr) {
+            if (element < min) {
+                min = element;
             }
-            while (low < high && intArr[low] <= pivot) {
-                low++;
+            if (element > max) {
+                max = element;
             }
-            if (low >= high) {
-                break;
-            }
-            int tmp = intArr[low];
-            intArr[low] = intArr[high];
-            intArr[high] = tmp;
         }
-        intArr[start] = intArr[low];
-        intArr[low] = pivot;
-        return low;
+        //计数数组，统计原数组中每个元素存在多少个(做了偏移处理，偏移量为原数组的最小元素)
+        //原数组中的元素减去偏移量，作为计数数组的下标，原数组元素个数作为计数数组的元素
+        int[] countNumArr = new int[max - min + 1];
+        for (int element : intArr) {
+            countNumArr[element - min]++;
+        }
+        //前缀和数组，统计原数组中某个元素及其前面的元素总共多少个
+        for (int i = 1; i < countNumArr.length; i++) {
+            countNumArr[i] = countNumArr[i] + countNumArr[i - 1];
+        }
+        //零时排序数组
+        int[] tmpSortArr = new int[length];
+        //从后往前取出原数组中的元素
+        //作为前缀和数组的下标
+        //取出前缀和数组对应的元素
+        //该元素值就是原数组元素对应零时排序数组的位置
+        for (int i = length - 1; i >= 0; i--) {
+            int element = intArr[i];
+            int tmpSortArrIndex = countNumArr[element - min]-1;
+            tmpSortArr[tmpSortArrIndex] = element;
+        }
+        System.arraycopy(tmpSortArr,0,intArr,0,length);
     }
-
 
     public static void main(String[] args) {
-        int[] source = {2, 3, 1, 5, 4};
+        int[] source = {5, 20, 4, 3, 1};
         sortIntArrayAsc(source);
         System.out.println(Arrays.toString(source));
     }
