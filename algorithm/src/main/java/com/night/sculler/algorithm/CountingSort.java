52840a2d79b029570d16eecaca3e354ea4d66061
houlongjie <houlongjie@jd.com>
2021-10-09 16:19:34 +0800

    计数排序

--- algorithm/src/main/java/com/night/sculler/algorithm/CountingSort.java
+++ algorithm/src/main/java/com/night/sculler/algorithm/CountingSort.java
@@ -20,7 +20,7 @@ public class CountingSort {
         if (length <= 1) {
             return;
         }
-        //求出最大最小元素
+        //首先求出数组的最大最小值
         int min = intArr[0];
         int max = intArr[0];
         for (int element : intArr) {
@@ -31,28 +31,27 @@ public class CountingSort {
                 max = element;
             }
         }
-        //计数数组，统计原数组中每个元素存在多少个(做了偏移处理，偏移量为原数组的最小元素)
-        //原数组中的元素减去偏移量，作为计数数组的下标，原数组元素个数作为计数数组的元素
+        //计数数组，下标范围为（min - min）~（max - min ）
+        //其含义为：一个连续的且覆盖住原始数组所有元素的集合，经过偏移后就是计数数组的下标；
+        //计数数组的下标加上偏移量后，和原始数组中的元素形成对应关系。计数数组的元素就是其下标对应的原始数组中的元素的个数
+        //其中min为偏移量，为了减少空间、兼容负数、兼容较大的数据集合
         int[] countNumArr = new int[max - min + 1];
         for (int element : intArr) {
             countNumArr[element - min]++;
         }
-        //前缀和数组，统计原数组中某个元素及其前面的元素总共多少个
+        //前缀和数组，每个元素的值是计数数组对应值和该对应值前面所有值相加
+        //其含义就为：前缀和数组下标在有序的情况下，原始数组对应元素在第几个
         for (int i = 1; i < countNumArr.length; i++) {
             countNumArr[i] = countNumArr[i] + countNumArr[i - 1];
         }
-        //零时排序数组
+        //零时有序数组，从后往前用原始数组的元素减去偏移量作为下标，到前缀和数组中取值，该值就是原始数组元素在有序数组中的位置
         int[] tmpSortArr = new int[length];
-        //从后往前取出原数组中的元素
-        //作为前缀和数组的下标
-        //取出前缀和数组对应的元素
-        //该元素值就是原数组元素对应零时排序数组的位置
         for (int i = length - 1; i >= 0; i--) {
             int element = intArr[i];
-            int tmpSortArrIndex = countNumArr[element - min] - 1;
-            tmpSortArr[tmpSortArrIndex] = element;
+            int tempSortIndex = countNumArr[element - min] - 1;
+            tmpSortArr[tempSortIndex] = element;
         }
-        System.arraycopy(tmpSortArr, 0, intArr, 0, length);
+        System.arraycopy(tmpSortArr,0,intArr,0,length);
     }
 
     public static void main(String[] args) {
