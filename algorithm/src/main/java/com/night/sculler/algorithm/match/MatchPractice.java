8c31abeb731f1b21510a15129c7735cfee3fc6cc
houlongjie <houlongjie@jd.com>
2021-11-11 16:17:40 +0800

    字符串匹配

--- algorithm/src/main/java/com/night/sculler/algorithm/match/MatchPractice.java
+++ algorithm/src/main/java/com/night/sculler/algorithm/match/MatchPractice.java
@@ -1,30 +1,21 @@
 package com.night.sculler.algorithm.match;
 
 /**
- * BF字符串匹配算法
- *
- * @author night-sculler
- * @version 1.0.0.release
- * @date 2021/10/17
+ * @author houlongjie
+ * @version 1.0
+ * @date 2021/10/22
  */
-public class BfStringMatch {
-    /**
-     * 在原字符串中匹配目标字符串并返回第一次匹配到时首字母对应的下标
-     *
-     * @param source 原数组
-     * @param target 目标数组
-     * @return 第一次匹配到时首字母对应的下标
-     */
+public class MatchPractice {
+
     public static int match(String source, String target) {
         if (null == source || null == target) {
             return 0;
         }
         int sLen = source.length();
         int tLen = target.length();
-        if (sLen < tLen) {
+        if (tLen > sLen) {
             return -1;
         }
-        //按顺序将目标字符串中的字符和原字符串进行匹配，匹配到了就接着匹配，匹配不到重新开始和原字符串的下一个字符匹配
         for (int i = 0; i < sLen - tLen + 1; i++) {
             int j;
             for (j = 0; j < tLen; j++) {
@@ -40,7 +31,7 @@ public class BfStringMatch {
     }
 
     public static void main(String[] args) {
-        int match = match("hello", "ll");
+        int match = match("hello", "lo");
         System.out.println(match);
     }
 }
